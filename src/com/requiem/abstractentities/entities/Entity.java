75e7a4f0d89c25b0e413156b1a546e8a8fe582b9
Matthew Laux <mlaux@evernote.com>
2014-11-27 21:13:21 -0600

    updated entity with collision

--- src/com/requiem/abstractentities/entities/Entity.java
+++ src/com/requiem/abstractentities/entities/Entity.java
@@ -1,13 +1,18 @@
 package com.requiem.abstractentities.entities;
 
 
+import com.bulletphysics.collision.shapes.CollisionShape;
 import com.requiem.abstractentities.AbstractEntity;
 import com.requiem.interfaces.Renderable;
+
+import java.util.List;
 
 /**
  * Created by Trent on 10/24/2014.
  */
 public class Entity extends AbstractEntity implements Renderable {
+    public List<CollisionShape> collisionShapes;
+
     public void init() {
 
     }
